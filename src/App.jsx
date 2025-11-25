7136b58e68159dab6965d2cf638d730d2e546846
Israr Ahmad Tech <israrahmadtech@gmail.com>
2025-11-25 14:19:54 +0500

    Loading Added App.jsx
    Loading Added App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,25 @@ import { supabase } from "./supabase-client"
 
 function App() {
   const [session, setSession] = useState(null)
-  const fetchSession = async () => {
-    const currentSession = await supabase.auth.getSession()
+const [loading, setLoading] = useState(true)
 
-    setSession(currentSession.data.session)
+const fetchSession = async () => {
+  const { data: { session } } = await supabase.auth.getSession()
+  setSession(session)
+  setLoading(false)
+}
+
+useEffect(() => {
+  fetchSession()
+
+  const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+    setSession(session)
+  })
+
+  return () => {
+    authListener.subscription.unsubscribe()
   }
-
-  useEffect(() => {
-    fetchSession()
-
-    const {data: authListner} = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-
-    return () => {
-      authListner.subscription.unsubscribe()
-    }
-  }, [])
+}, [])
 
   async function handleLogout(){
     const {error} = await supabase.auth.signOut({ scope: 'global' });
@@ -32,14 +34,16 @@ function App() {
   }
   return (
     <div className="p-10 w-full min-h-screen flex justify-center flex-col items-center bg-[#212121] text-white">
-      {!session ?
-          <Authentication />
-        :
-        <>
-          <button onClick={handleLogout} className="mb-5 bg-black py-2 px-4 rounded-xl font-semibold cursor-pointer border border-transparent hover:border-purple-500 transition">Logout</button>
-          <TaskManager session={session} />
-        </>
-      }
+      {loading ? (
+      <p>Loading...</p>
+    ) : !session ? (
+      <Authentication />
+    ) : (
+      <>
+        <button onClick={handleLogout} className="mb-5 bg-black py-2 px-4 rounded-xl font-semibold cursor-pointer border border-transparent hover:border-purple-500 transition">Logout</button>
+        <TaskManager session={session} />
+      </>
+    )}
     </div>
   )
 }
