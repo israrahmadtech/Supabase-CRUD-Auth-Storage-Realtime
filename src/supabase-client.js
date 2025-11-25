84d938877b20d4c69b671d3adcaae0b127e91ca9
Israr Ahmad Tech <israrahmadtech@gmail.com>
2025-11-25 14:03:28 +0500

    Update supabase-client.js
    Danger console removed

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -3,8 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const projectUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL
 const apiKey = import.meta.env.VITE_SUPABASE_API_KEY
-console.log(projectUrl);
-console.log(apiKey);
-
 
 export const supabase = createClient(projectUrl, apiKey)
