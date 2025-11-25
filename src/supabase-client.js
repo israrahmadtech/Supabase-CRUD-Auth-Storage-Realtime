ce46924fb1308ea582adf46b20c5486bcf0dc555
israrahmadtech <israrahmadtech@gmail.com>
2025-11-25 13:37:27 +0500

    env added

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const projectUrl = "https://cqpiwlbmgpqwviupuoba.supabase.co"
-const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxcGl3bGJtZ3Bxd3ZpdXB1b2JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MjY5MTAsImV4cCI6MjA3OTQwMjkxMH0.WTvYHU27DNeN8ofR0NZtfJsbEFnLcGsK_tLVjPoFY2s"
+
+const projectUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL
+const apiKey = import.meta.env.VITE_SUPABASE_API_KEY
+console.log(projectUrl);
+console.log(apiKey);
+
 
 export const supabase = createClient(projectUrl, apiKey)
